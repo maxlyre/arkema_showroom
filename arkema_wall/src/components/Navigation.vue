1598cba8e5beee16d19d780d86b3451f17d2345a
maxlyre <m.vermesch@gmail.com>
2022-04-21 01:12:48 +0200

    add lang switch

--- arkema_wall/src/components/Navigation.vue
+++ arkema_wall/src/components/Navigation.vue
@@ -8,7 +8,7 @@
     },
     data() {
       return {
-
+        lang:"en",
       }
     },
     methods:{
@@ -36,7 +36,13 @@
           document.querySelector('.active').classList.remove("active")
           divs[id].classList.add("active")
           
-        }
+        },
+        changeLang(lang){
+          this.lang = lang;
+        },
+    },
+    mounted(){
+      console.log(this.content)
     }
   };
 </script>
@@ -52,17 +58,30 @@
           </ul>
         </li>
         <li v-for="nav in content">
-          {{ nav.Title }}
+          <div v-if="lang=='fr'" class="group_title">
+                {{ nav.Title }}
+          </div>
+          <div v-else class="group_title">
+                {{ nav.EnglishTitle }}
+          </div>
           <ul>
-            <li v-for="data in nav.walls_items.data">
-              <a :targetID="data.id" @click="changeIndex(data.id)">{{data.attributes.Title}}</a>
-            </li>  
+            <template v-for="data in nav.walls_items.data">
+              <li v-if="data.attributes.localizations.data.length>0 || lang=='fr'">
+                <a  v-if="lang=='fr'" :targetID="data.id" @click="changeIndex(data.id)">{{data.attributes.Title}}</a>
+                <a  v-else :targetID="data.attributes.localizations.data[0].id" @click="changeIndex(data.attributes.localizations.data[0].id)">{{data.attributes.localizations.data[0].attributes.Title}}</a>
+              </li>  
+            </template>
+
           </ul>
         </li>
       </ul>
       <div class="navigation">
           <button class="prev" @click="arrowNav(-1)">Prev</button>
           <button class="next" @click="arrowNav(1)">Next</button>    
+      </div>
+      <div class="lang_switcher">
+          <button class="fr" @click="changeLang('fr')">FR</button>
+          <button class="en" @click="changeLang('en')">EN</button>    
       </div>
     </div>
 </template>
