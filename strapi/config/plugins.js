8a97a2f017d55ed44f1cbe1834f6821d999de057
maxlyre <m.vermesch@gmail.com>
2022-08-10 19:22:00 +0200

    update strapi + change ckeditor5 custom

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,8 +1,9 @@
 // path: ./config/plugins.js
 
-module.exports = {
+module.exports = () => ({
     //
     graphql: {
+      enabled: true,
       config: {
         endpoint: '/graphql',
         shadowCRUD: true,
@@ -14,5 +15,55 @@ module.exports = {
         },
       },
     },
-  };
+    ckeditor: {
+      enabled: true,
+      config:{
+        plugin:{
+          // strapiTheme:false,
+          styles:`
+            .ck.ck-editor__main>.ck-editor__editable {
+              background: #ededf3 !important;
+            }
+          `
+        },
+        editor:{
+          toolbar:{
+            items: [
+              'paragraph',
+              'heading2',
+              'heading4',
+              'fontSize',
+              '|',
+              'bold',
+              'italic',
+              'underline',
+              'superscript',
+              'alignment',
+              'specialCharacters',
+              'removeFormat',
+              '|',
+              'bulletedList',
+              'numberedList',
+              '|',
+              'StrapiMediaLib',
+              'insertTable',
+              'link',
+              '|',
+              'sourceEditing',
+            ]
+          },
+          fontSize: {
+            options: [
+              { model: 'tiny', title: 'Tiny', class: 'text-tiny' },
+              { model: 'small', title: 'Small', class: 'text-small' },
+              'default',
+              { model: 'big', title: 'Big', class: 'text-big' },
+              { model: 'huge', title: 'Huge', class: 'text-huge' },
+            ],
+            supportAllValues: false
+          },
+        }
+      }
+    },
+  });
    
